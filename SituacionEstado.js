c0f6552715457027e3e45592eb5c1d0c1d82eff5
C3sarCruz <106643035+C3sarCruz@users.noreply.github.com>
2024-02-20 13:16:05 -0600

    Initial commit

---
+++ SituacionEstado.js
@@ -0,0 +1,183 @@
+
+function muestra_ActCir(id) {
+        if (document.getElementById) { 
+            var el = document.getElementById(id);
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none'; 
+        }
+    }
+    window.onload = function () {
+        muestra_ActCir('contenidoAC');
+}
+
+function cerrarAC() {
+    document.getElementById("contenidoAC").style.display = "none";
+}
+
+ 
+    function muestra_PasCir(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+    window.onload = function () {
+        muestra_PasCir('contenidoPC');
+}
+
+function cerrarPC() {
+    document.getElementById("contenidoPC").style.display = "none";
+}
+
+
+    function muestra_ActNoCir(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+    window.onload = function () {
+        muestra_ActNoCir('contenidoANC');
+    }
+
+function cerrarANC() {
+    document.getElementById("contenidoANC").style.display = "none";
+}
+
+
+
+
+    function muestra_PasNoCir(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+    window.onload = function () {
+        muestra_PasNoCir('contenidoPNC');
+}
+
+
+function cerrarPNC() {
+    document.getElementById("contenidoPNC").style.display = "none";
+}
+
+
+    function muestra_oActNoCir(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+    window.onload = function () {
+        muestra_oActNoCir('contenidoOANC');
+}
+
+
+function cerrarOANC() {
+    document.getElementById("contenidoOANC").style.display = "none";
+}
+
+
+        function muestra_P(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_P('contenidoP');
+}
+
+function cerrarP() {
+    document.getElementById("contenidoP").style.display = "none";
+}
+
+function muestra_CB(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_CB('contenidoCB');
+}
+
+function cerrarCB() {
+    document.getElementById("contenidoCB").style.display = "none";
+}
+
+function muestra_CBB1(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_CBB1('contenidoCBB1');
+}
+
+function cerrarCBB1() {
+    document.getElementById("contenidoCBB1").style.display = "none";
+}
+
+
+function muestra_CBB2(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_CBB2('contenidoCBB2');
+}
+
+function cerrarCBB2() {
+    document.getElementById("contenidoCBB2").style.display = "none";
+} 
+function muestra_TPasNC(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_TPasNC('contenidoTPasNC');
+}
+
+function cerrarTPasNC() {
+    document.getElementById("contenidoTPasNC").style.display = "none";
+}
+
+function muestra_PG(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_PG('contenidoPG');
+}
+
+function cerrarPG() {
+    document.getElementById("contenidoPG").style.display = "none";
+}
+
+
+
+/* const openModalAC = document.querySelector('.desplegar');
+const modalAC = document.querySelector('.modalAC');
+const closeModalAC = document.querySelector('.cerrar');
+
+openModalAC.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modalAC.classList.add('modal--show');
+});
+
+closeModalAC.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modalAC.classList.remove('modal--show');
+});
+ */
+
+
+
