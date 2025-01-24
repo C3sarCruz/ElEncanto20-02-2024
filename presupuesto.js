2e1d1b9fe5fc6738fe42c686c066a5f9946934f2
Cruz Cesar <106643035+C3sarCruz@users.noreply.github.com>
2025-01-23 20:43:22 -0600

    Segundo avance para revisión
    En este commit se agregaron los datos de el apartado presupuesto y la grafica de resultados 2025

--- presupuesto.js
+++ presupuesto.js
@@ -20,23 +20,23 @@ function cancelFullScreen() {
 
 
 function muestraP(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestra_P('contenidoP');
+}
+window.onload = function () {
+    muestra_P('contenidoP');
 }
 
 function muestraI(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraI('contenidoI');
+}
+window.onload = function () {
+    muestraI('contenidoI');
 }
 function cerrarI() {
     document.getElementById("contenidoI").style.display = "none";
@@ -44,13 +44,13 @@ function cerrarI() {
 
 
 function muestraG(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraG('contenidoG');
+}
+window.onload = function () {
+    muestraG('contenidoG');
 }
 function cerrarG() {
     document.getElementById("contenidoG").style.display = "none";
@@ -58,89 +58,89 @@ function cerrarG() {
 
 
 function muestraGA(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraGA('contenidoGA');
+}
+window.onload = function () {
+    muestraGA('contenidoGA');
 }
 function cerrarGA() {
     document.getElementById("contenidoGA").style.display = "none";
 }
 
 function muestraGM(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraGM('contenidoGM');
+}
+window.onload = function () {
+    muestraGM('contenidoGM');
 }
 function cerrarGM() {
     document.getElementById("contenidoGM").style.display = "none";
 }
 function muestraME(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraME('contenidoME');
+}
+window.onload = function () {
+    muestraME('contenidoME');
 }
 function cerrarME() {
     document.getElementById("contenidoME").style.display = "none";
 }
 
 function muestraA(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraA('contenidoA');
+}
+window.onload = function () {
+    muestraA('contenidoA');
 }
 function cerrarA() {
     document.getElementById("contenidoA").style.display = "none";
 }
 
 function muestraJ(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraJ('contenidoJ');
+}
+window.onload = function () {
+    muestraJ('contenidoJ');
 }
 function cerrarJ() {
     document.getElementById("contenidoJ").style.display = "none";
 }
 
 function muestraS(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraS('contenidoS');
+}
+window.onload = function () {
+    muestraS('contenidoS');
 }
 function cerrarS() {
     document.getElementById("contenidoS").style.display = "none";
 }
 function muestraE(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraE('contenidoE');
+}
+window.onload = function () {
+    muestraE('contenidoE');
 }
 function cerrarE() {
     document.getElementById("contenidoE").style.display = "none";
@@ -148,15 +148,29 @@ function cerrarE() {
 
 
 function muestraPremisas(id) {
-        if (document.getElementById) {
-            var el = document.getElementById(id);
+    if (document.getElementById) {
+        var el = document.getElementById(id);
         el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
     }
-        window.onload = function () {
-            muestraE('contenidoPremisas');
+}
+window.onload = function () {
+    muestraE('contenidoPremisas');
 }
 
 function cerrarPremisas() {
     document.getElementById("contenidoPremisas").style.display = "none";
+}
+
+function muestraSP(id) {
+    if (document.getElementById) {
+        var el = document.getElementById(id);
+        el.style.display = el.style.display == "none" ? "block" : "none";
+    }
+}
+window.onload = function () {
+    muestraSP("contenidoSP");
+};
+
+function cerrarSP() {
+    document.getElementById("contenidoSP").style.display = "none";
 }
